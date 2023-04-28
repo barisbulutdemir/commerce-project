9e56b269476bc99feea9947ea4d969389fc7fb9f
barisbulutdemir <barisbulutdemir@gmail.com>
2023-04-28 21:40:39 +0200

    sss

--- src/components/utils/Clips.jsx
+++ src/components/utils/Clips.jsx
@@ -3,16 +3,15 @@ import React from "react";
 const Clips = (clip, imgsrc) => {
     return (
         <>
-        <div className="relative h-28 w-32">
-            <img src = {imgsrc}
+        <div className="relative h-28 w-32 rounded-xl overflow-hidden">
+            <img
+            src={imgsrc}
             alt="img/clips"
-            className="inset-0 flex h-full w-full object-cover absolute"/>
-
+            className=" inset-0 w-full h-full object-cover absolute
+            top-0 left-0 right-0 rounded-xl opacity-100 z-10"/>
             <video>
-                <source type="video/mp4"
-                        src = {clip} />
+                <source type="video/mp4" src={clip}/>
             </video>
-
         </div>
         </>
     )
