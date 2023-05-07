cb78ee35d3f33435e196b6e47c6cbd8546617916
barisbulutdemir <barisbulutdemir@gmail.com>
2023-05-07 11:31:04 +0200

    sa
    sa

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 
 import './App.css'
-import { Hero, Sales} from './components'
-import { heroapi, popularsales, toprateslaes } from "./data/data.js";
+import { FlexContent, Hero, Sales, Stories} from './components'
+import { heroapi, popularsales, toprateslaes, highlight, sneaker , story} from "./data/data.js";
 
 
 function App() {
@@ -11,14 +11,16 @@ function App() {
     <>
       <main className='flex flex-col gap-16 relative'>
           <Hero heroapi ={heroapi}   />
-
           {/* Neden 2 kere Sales kullandık ? Çünkü tek template üstünden 
           2 ayrı şey göstermek istiyoruz. Tekrar altta ayrı bir template oluşturmak istemiyoruz*/}
           <Sales endpoint = {popularsales} ifExists />
           {/* Yukarıda ki ifexists in sebebi: Bunu bi prop olarak gönderip,
            eğer papularsaleslar farklı bir biçimde olacaksa  if metoduna alıp ona göre ayarlayabiliriz */}
+          <FlexContent endpoint={highlight } ifExists />
 
           <Sales endpoint = {toprateslaes }/>
+          <FlexContent endpoint={sneaker} />
+          <Stories story={story} />
 
     </main>
         </>
--- src/components/index.js
+++ src/components/index.js
@@ -1,3 +1,6 @@
 export { default as Hero } from './hero.jsx'
 
 export { default as Sales} from './Sales.jsx'
+
+export { default as FlexContent } from './FlexContent.jsx'
+export { default as Stories } from './Stories.jsx'
